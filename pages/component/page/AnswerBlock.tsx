489e68e5838b22d34602df37d3821803fb12cb73
Kekelberi <alexei.gaponov@gmail.com>
2023-06-18 12:57:03 -0500

    accordion

--- pages/component/page/AnswerBlock.tsx
+++ pages/component/page/AnswerBlock.tsx
@@ -1,21 +1,36 @@
+"use client"
+import { useState } from "react"
 import s from "../../../styles/main.module.scss"
 
 const AnswerBlock = () => {
+  const [isActive1, setIsActive1] = useState(false)
+  const [isActive2, setIsActive2] = useState(false)
+  const [isActive3, setIsActive3] = useState(false)
+  const [isActive4, setIsActive4] = useState(false)
+  const [isActive5, setIsActive5] = useState(false)
+  const [isActive6, setIsActive6] = useState(false)
+
   return (
     <div className={s.answerWrapper}>
       <div>
         <div className="hs-accordion-group">
           <div className="hs-accordion active" id="hs-basic-heading-one">
             <button
-              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+              className="hs-accordion-toggle hs-accordion-active:text-blue-600
+               group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left
+                text-gray-400 transition hover:text-gray-100 dark:hs-accordion-active:text-blue-500
+                 dark:text-gray-100 dark:hover:text-gray-100"
               aria-controls="hs-basic-collapse-one"
+              onClick={() => setIsActive1(!isActive1)}
             >
               <ElementSvg />
               Accordion #1
             </button>
             <div
               id="hs-basic-collapse-one"
-              className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+              className={`${
+                isActive1 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
               aria-labelledby="hs-basic-heading-one"
             >
               <div>
@@ -31,15 +46,21 @@ const AnswerBlock = () => {
 
           <div className="hs-accordion" id="hs-basic-heading-two">
             <button
-              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+              className="hs-accordion-toggle hs-accordion-active:text-blue-600
+               group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left
+                text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500
+                 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-collapse-two"
+              onClick={() => setIsActive2(!isActive2)}
             >
               <ElementSvg />
               Accordion #2
             </button>
             <div
               id="hs-basic-collapse-two"
-              className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+              className={`${
+                isActive2 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
               aria-labelledby="hs-basic-heading-two"
             >
               <p className="text-gray-800 dark:text-gray-200">
@@ -56,13 +77,94 @@ const AnswerBlock = () => {
             <button
               className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-collapse-three"
+              onClick={() => setIsActive3(!isActive3)}
             >
               <ElementSvg />
               Accordion #3
             </button>
             <div
               id="hs-basic-collapse-three"
-              className="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+              className={`${
+                isActive3 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
+              aria-labelledby="hs-basic-heading-three"
+            >
+              <p className="text-gray-800 dark:text-gray-200">
+                <em>This is the third item accordion body.</em> It is hidden by
+                default, until the collapse plugin adds the appropriate classes
+                that we use to style each element. These classes control the
+                overall appearance, as well as the showing and hiding via CSS
+                transitions.
+              </p>
+            </div>
+          </div>
+          
+          <div className="hs-accordion" id="hs-basic-heading-three">
+            <button
+              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+              aria-controls="hs-basic-collapse-three"
+              onClick={() => setIsActive4(!isActive4)}
+            >
+              <ElementSvg />
+              Accordion #4
+            </button>
+            <div
+              id="hs-basic-collapse-three"
+              className={`${
+                isActive4 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
+              aria-labelledby="hs-basic-heading-three"
+            >
+              <p className="text-gray-800 dark:text-gray-200">
+                <em>This is the third item accordion body.</em> It is hidden by
+                default, until the collapse plugin adds the appropriate classes
+                that we use to style each element. These classes control the
+                overall appearance, as well as the showing and hiding via CSS
+                transitions.
+              </p>
+            </div>
+          </div>
+
+          <div className="hs-accordion" id="hs-basic-heading-three">
+            <button
+              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+              aria-controls="hs-basic-collapse-three"
+              onClick={() => setIsActive5(!isActive5)}
+            >
+              <ElementSvg />
+              Accordion #5
+            </button>
+            <div
+              id="hs-basic-collapse-three"
+              className={`${
+                isActive5 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
+              aria-labelledby="hs-basic-heading-three"
+            >
+              <p className="text-gray-800 dark:text-gray-200">
+                <em>This is the third item accordion body.</em> It is hidden by
+                default, until the collapse plugin adds the appropriate classes
+                that we use to style each element. These classes control the
+                overall appearance, as well as the showing and hiding via CSS
+                transitions.
+              </p>
+            </div>
+          </div>
+
+          <div className="hs-accordion" id="hs-basic-heading-three">
+            <button
+              className="hs-accordion-toggle hs-accordion-active:text-blue-600 group py-3 inline-flex items-center gap-x-3 w-full font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:hs-accordion-active:text-blue-500 dark:text-gray-200 dark:hover:text-gray-400"
+              aria-controls="hs-basic-collapse-three"
+              onClick={() => setIsActive6(!isActive6)}
+            >
+              <ElementSvg />
+              Accordion #6
+            </button>
+            <div
+              id="hs-basic-collapse-three"
+              className={`${
+                isActive6 ? "" : "hidden"
+              } hs-accordion-content  w-full overflow-hidden transition-[height] duration-300`}
               aria-labelledby="hs-basic-heading-three"
             >
               <p className="text-gray-800 dark:text-gray-200">
